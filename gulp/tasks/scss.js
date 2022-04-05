5d1cf9f69786bec2a4a9cbeb81a5e89e9272cf54
syniuk-a <78295684+syniuk-a@users.noreply.github.com>
2022-04-05 13:05:54 +0300

    comit
    оставить на позже

--- gulp/tasks/scss.js
+++ gulp/tasks/scss.js
@@ -16,7 +16,7 @@ export const scss = () => {
                 title: "SCSS",
                 message: "Error: <%= error.message %>"
             })))
-        .pipe(app.plugins.replace(/@img\//g, '../img/'))
+        .pipe(app.plugins.replace(/@img\//g, '..img/'))
         .pipe(sass({
             outputStyle: 'expanded'
         }))
